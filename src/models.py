8913e233bcde90e8c77fb681e4942f787d506ca0
alfonsoperezs <fonpesan@gmail.com>
2025-04-13 21:25:06 +0200

    Add class message

--- src/models.py
+++ src/models.py
@@ -1,10 +1,15 @@
 from sqlmodel import Field, SQLModel
 from datetime import datetime
 
+class Message(SQLModel):
+    detail: str
+
 class Delay(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
     expedition_id: int = Field(default=None, nullable=False)
     line_name: str = Field(default=None, nullable=False)
     real_time: datetime = Field(nullable=False)
     passing_time: datetime = Field(default=None, nullable=False)
+    stop_id: int = Field(default=None, nullable=False)
+    stop_name: str = Field(default=None, nullable=False)
 
